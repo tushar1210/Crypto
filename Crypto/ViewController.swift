4adb96ccaaaf89017907b173301362bad240429f
tushar1210 <ts26192271@gmail.com>
2018-05-23 01:31:22 +0530

    Initial Commit

--- Crypto/ViewController.swift
+++ Crypto/ViewController.swift
@@ -7,12 +7,63 @@
 //
 
 import UIKit
-
+import Alamofire
+import SwiftyJSON
 class ViewController: UIViewController {
 
+   /////////////////////////outlets///////////////////////////
+    
+    @IBOutlet weak var value: UILabel!
+    let url : String = "https://api.cryptonator.com/api/ticker/"
+    
+   
+    @IBAction func coinButton(_ sender: AnyObject) {
+        let check:Int = sender.tag
+        print(url+typeArray[check-1])
+        
+        getValue(finalUrl:url+typeArray[check-1])
+    }
+    
+    
+    let typeArray = ["btc-usd","ltc-usd"]
+////////////////////////////////////////////?????///////////////////////////////
+    func getValue(finalUrl:String){
+        
+        Alamofire.request(finalUrl, method: .get).responseJSON{
+                response in
+                if response.result.isSuccess{
+                    print("Crypto Data Retrieved")
+                    let json : JSON = JSON(response.result.value!)
+                    self.JSONParsing(json:json)
+                }
+                else{
+                    print("Error : \(String(describing: response.result.error))")
+                    self.value.text = "Connection Issues"
+                }
+            }
+        
+        
+    }
+    
+    func JSONParsing(json:JSON){
+                     if let results = json["ticker"]["price"].double{
+                    print(results)
+                    print("Success")
+                        value.text = String(results)
+                }
+                else{
+                    
+                        value.text="Data Unavailable"
+                }
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        value.text = "Choose Coin"
+        
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +73,3 @@ class ViewController: UIViewController {
 
 
 }
-
